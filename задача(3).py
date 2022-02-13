0fe8000af216e4a2c93a59ea571c530540708a8a
Ramazan0509 <97314639+Ramazan0509@users.noreply.github.com>
2022-02-13 16:18:11 +0300

    Delete задача(3).py

--- задача(3).py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Magomedov Ramazan'
-# Задача-3: Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
-# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
-number = input("Введите число: ")
-a = int(number + number)
-b = int(number+number+number)
-summa = int(number) + a + b
-
-print(summa)

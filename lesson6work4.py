aeadba33852111c89855a17d2f14786b1e3a9c25
Ramazan0509 <97314639+Ramazan0509@users.noreply.github.com>
2022-05-21 20:32:08 +0300

    Update lesson6work4.py

--- lesson6work4.py
+++ lesson6work4.py
@@ -1,46 +1,63 @@
 __author__ = 'Magomedov Ramazan'
-#Реализовать класс Stationery (канцелярская принадлежность).
-#определить в нём атрибут title (название) и метод draw (отрисовка).
-# Метод выводит сообщение «Запуск отрисовки»;
-#создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер);
-#в каждом классе реализовать переопределение метода draw. Для каждого класса
-# метод должен выводить уникальное сообщение;
-#создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
-class Stationery:
-    title = "\n<< Канцелярская принадлежность >>"
-
-    def draw(self):
-        print("Родительский метод класса Stationery")
-        print("Запуск отрисовки")
+"""Реализуйте базовый класс Car. У данного класса должны быть следующие атрибуты: speed, color, name, is_police (
+булево).  А также методы: go, stop, turn(direction), которые должны сообщать, что машина поехала, остановилась,
+повернула (куда). Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar. Добавьте в базовый класс
+метод show_speed, который должен показывать текущую скорость автомобиля. Для классов TownCar и WorkCar переопределите
+метод show_speed. При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться сообщение о превышении
+скорости. Создайте экземпляры классов, передайте значения атрибутов. Выполните доступ к атрибутам,
+выведите результат. Выполните вызов методов и также покажите результат. """
 
 
-class Pen(Stationery):
-    def draw(self):
-        print("\nРодительский метод класса Pen")
-        print("Запуск отрисовки ручкой")
+class Car:
+    def __init__(self, speed: int, color: str, name: str):
+        self.speed = speed
+        self.color = color
+        self.name = name
+
+    def go(self):
+        print('Вперед')
+
+    def stop(self):
+        print('Стоп')
+
+    def turn(self, side):
+        print(side)
+
+    def show_speed(self):
+        print(f'Скорость : {self.speed}')
 
 
-class Pencil(Stationery):
-    def draw(self):
-        print("\nРодительский метод класса Pencil")
-        print("Запуск отрисовки карандашом")
+class TownCar(Car):
+    def show_speed(self):
+        super().show_speed()
+        if self.speed > 60:
+            print(f"Превышена скорость на {self.speed - 60}")
 
 
-class Handle(Stationery):
-    def draw(self):
-        print("\nРодительский метод класса Handle")
-        print("Запуск отрисовки маркером")
+class SportCar(Car):
+    pass
 
 
-s = Stationery()
-print(s.title)
-s.draw()
+class WorkCar(Car):
+    def show_speed(self):
+        super().show_speed()
+        if self.speed > 40:
+            print(f"Превышена скорость на {self.speed - 40}")
 
-p_1 = Pen()
-p_1.draw()
 
-p_2 = Pencil()
-p_2.draw()
+class PoliceCar(Car):
+    def __init__(self, speed: int, color: str, name: str):
+        super().__init__(speed, color, name)
+        self.is_police = True
 
-h = Handle()
-h.draw()
+
+if __name__ == '__main__':
+
+    for car in [TownCar(55, 'красная', 'Toyota'), SportCar(80, 'белая', 'Nissan'), WorkCar(50, 'коричневая', 'Ford'),
+                PoliceCar(90, 'синяя', 'Ford')]:
+        print('---')
+        print(car.name, car.color, car.speed)
+        car.show_speed()
+        car.go()
+        car.turn('налево')
+        car.stop()
